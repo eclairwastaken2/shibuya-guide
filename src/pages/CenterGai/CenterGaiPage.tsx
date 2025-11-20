ef7743f955194bfa100578a736c2e8f6c904997f
eclairwastaken2 <merlinthio123+505not404@gmail.com>
2025-11-20 22:40:50 +0700

    Add back button

--- src/pages/CenterGai/CenterGaiPage.tsx
+++ src/pages/CenterGai/CenterGaiPage.tsx
@@ -6,6 +6,7 @@ import SlideInit from "./SlideInit"
 import SlideFashion from "./SlideFashion"
 import SlideFood from "./SlideFood"
 import SlideTravel from "./SlideTravel"
+import { Link } from "react-router-dom";
 import React from "react"
 
 export default function CenterGaiPage() {
@@ -24,9 +25,10 @@ export default function CenterGaiPage() {
                 <Neon onClick={() => setCurrentIndex(1)} textColor="#fff6d9" bgColor="#e62e00" font="var(--font-anton)" header="FOOD" className={activeSlide === 1 ? "scale-115" : "scale-100"} />
                 <Neon onClick={() => setCurrentIndex(2)} bgColor="#16d0f5" textColor="white" font="var(--font-momo)" header="SHOP" className={activeSlide === 2 ? "scale-115" : "scale-100"} />
                 <Neon onClick={() => setCurrentIndex(3)} textColor="white" bgColor="#ff17f0" font="var(--font-neon)" header="TRAVEL" className={activeSlide === 3 ? "scale-115" : "scale-100"} />
-                    
+                <Link to={"/"} className="block z-20 text-white text-xl font-anton">
+                    <div>back</div>
+                </Link>
             </div>
-
         </div>
     )
 }
